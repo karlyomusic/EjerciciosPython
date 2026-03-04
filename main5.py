5222aaec96b3d8f6de7fe0d39c8361aedd0eaca0
karla paola osio amaya <osiokarla@gmail.com>
2026-03-04 17:00:27 -0500

    doc: Se realizó el ejercicio 4

---
+++ main5.py
@@ -0,0 +1,7 @@
+A = int(input("Ingrese un número: "))
+B = int(input("Ingrese un número: "))
+
+print("El resultado de la suma es", A + B)
+print("Su resultado restado es", A - B)
+print("Su resultado multiplicado es", A * B )
+print("Su resultado en division es", A / B)
